649357d37f07262d5c7a6cfe70892e8d7902e265
icemoon2026 <icemoon2026@126.com>
2026-02-03 00:32:29 +0800

    During program initialization, the program reads a local task list file and serializes it into task data; it also supports serializing input tasks and storing them locally.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,29 @@
 use clap::{arg, command, Command};
+use std::{fs, env, path::Path};
+use serde::{ Serialize, Deserialize };
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Task {
+    name: String,
+}
 
 fn main() {
-    let mut tasks: Vec<String> = Vec::new();
+    // Test code
+    // let data = r#"[
+    //     {"name": "吃饭"},
+    //     {"name": "睡觉"}
+    // ]"#;
+    // let tasks: Vec<Task> = serde_json::from_str(data).expect("JSON was not well-formatted");
+    // println!("{:?}", tasks);
+
+
+    let path = "./task_list.json";
+    if !Path::new(path).exists() {
+        fs::write(path, "[]").expect("Write failed!");
+    }
+
+    let contents = fs::read_to_string(path);
+    let mut tasks: Vec<String> = serde_json::from_str(contents.unwrap().as_str()).unwrap();
 
     let matches = command!() // requires `cargo` feature
         .propagate_version(true)
@@ -26,6 +48,9 @@ fn main() {
         Some(("add", sub_matches)) => {
             let task_id: String = sub_matches.get_one::<String>("TaskID").unwrap().to_string();
             tasks.push(String::from(task_id));
+            let result = serde_json::to_string(&tasks);
+            println!("{:?}", result);
+            fs::write("task_list.json", result.unwrap()).unwrap();
         },
         Some(("list", _)) => {
             for task_id in tasks {
@@ -35,10 +60,8 @@ fn main() {
         Some(("done", sub_matches)) => {
             let task_no: String = sub_matches.get_one::<String>("TaskNo").unwrap().to_string();
             let task_no = task_no.parse::<i32>().unwrap();
-            println!("{}", task_no);
+            println!("{}", task_no)
         }
         _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
     }
-
-
 }
