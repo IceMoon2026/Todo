bc9094998778c50626d35fb7304f97cb3555de73
yueb <yueb@MacBook-Air.local>
2026-02-02 22:03:20 +0800

    Add command "done".

--- src/main.rs
+++ src/main.rs
@@ -15,17 +15,27 @@ fn main() {
         .subcommand(
             Command::new("list")
                 .about("List all todos"))
+        .subcommand(
+            Command::new("done")
+                .about("Finish done task")
+                .arg(arg!([TaskNo]).required(true)),
+        )
         .get_matches();
 
     match matches.subcommand() {
         Some(("add", sub_matches)) => {
-            let task_id: &String = sub_matches.get_one::<String>("TaskID").unwrap();
+            let task_id: String = sub_matches.get_one::<String>("TaskID").unwrap().to_string();
             tasks.push(String::from(task_id));
         },
         Some(("list", _)) => {
             for task_id in tasks {
                 println!("{}", task_id);
             }
+        },
+        Some(("done", sub_matches)) => {
+            let task_no: String = sub_matches.get_one::<String>("TaskNo").unwrap().to_string();
+            let task_no = task_no.parse::<i32>().unwrap();
+            println!("{}", task_no);
         }
         _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
     }
